bf389705e40129a2f1d61a4779e9e3744590268d
bqt1703in <kingbqt03@gmail.com>
2024-05-04 12:41:12 +0700

    Xoá controlller edit, xoá route edit product

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,3 @@
 module.exports.index = (req, res) => {
   res.render("client/pages/product/index.pug");
 };
-module.exports.edit = (req, res) => {
-  res.send("Edit");
-};
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -2,6 +2,5 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/client/product.controller");
 router.get("/", controller.index);
-router.get("/edit", controller.edit);
 
 module.exports = router;
